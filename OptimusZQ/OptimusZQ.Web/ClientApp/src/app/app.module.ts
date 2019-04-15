4b10c86eb9bd3431a5fc10b2732a4270b53297a8
Yevhenii TKACHENKO <ytkachenko@lulusoftware.com>
2019-04-15 14:47:58 +0300

    Added Login form

--- OptimusZQ/OptimusZQ.Web/ClientApp/src/app/app.module.ts
+++ OptimusZQ/OptimusZQ.Web/ClientApp/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+import { AuthenticationService } from './_services/authentication.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,9 +32,11 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
     ])
   ],
-  providers: [],
+  providers: [AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ OptimusZQ/OptimusZQ.Web/ClientApp/src/app/auth/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../_services/authentication.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+/** login component*/
+export class LoginComponent implements OnInit {
+  model: any = {};
+  loading = false;
+  returnUrl: string;
+
+  /** login ctor */
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) { }
+
+
+  ngOnInit() {
+    // reset login status
+    this.authService.logout();
+
+  }
+
+  login() {
+    debugger
+    this.loading = true;
+    this.authService.login(this.model.username, this.model.password)
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.loading = false;
+        });
+  }
+}
